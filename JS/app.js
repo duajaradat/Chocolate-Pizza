bad916374b1d528815fa5e5a9717728feace097c
duajaradat <duajaradat164@gmail.com>
2021-07-03 10:09:00 +0300

    just need js

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,10 @@
 
+'use strict'
+
+function clickMe(Element){
+    if (Element.checked){
+        document.getElementById("cbox1").style.textDecoration="line-through";
+    }else{
+        document.getElementById("cbox1").style.textDecoration="none";
+    }
 }
